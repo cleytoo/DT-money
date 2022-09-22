28b4a1f06f25fa8c58bf4afae0904d1b1bbd5ced
cleyton vinicius <cleytonterceiro@outlook.com>
2022-09-22 16:59:37 -0300

    🚧 Transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,9 +4,36 @@ import * as S from './styles'
 
 export const Transactions = () => {
   return (
-    <S.TransactionsContainer>
+    <div>
       <Header />
       <Summary />
-    </S.TransactionsContainer>
+
+      <S.TransactionsContainer>
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighLight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighLight variant="outcome">
+                  - R$ 59,00
+                </S.PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.TransactionsContainer>
+    </div>
   )
 }
