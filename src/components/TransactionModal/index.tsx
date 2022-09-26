7119741708b98871b3f50083fb45538b99a0a452
cleyton vinicius <cleytonterceiro@outlook.com>
2022-09-25 23:33:11 -0300

    🚧 transactionTypeButton

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as S from './styles'
 
 export const TransactionModal = () => {
@@ -16,6 +16,17 @@ export const TransactionModal = () => {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <S.TransactionType>
+            <S.TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </S.TransactionTypeButton>
+            <S.TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </S.TransactionTypeButton>
+          </S.TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
